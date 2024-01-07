80ce29c93d3b64008a856d95b2e622383015ffce
snipc <snipc.mail@proton.me>
2024-01-07 14:08:32 +0530

    Editor Error

--- src/app/new/page.jsx
+++ src/app/new/page.jsx
@@ -1,20 +1,28 @@
 "use client";
 import {TextField } from "@radix-ui/themes";
-import React from "react";
+import {React,useCallback,useState} from "react";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 
 export default function page() {
+
+  const onChangeDescription = useCallback((value) => {
+    setDescription(value)
+    console.log(description)
+  }, []);
+
+  let [title,setTitle]= useState("");
+  let [description,setDescription]= useState("")
   return (
     <>
    <h1 className="text-3xl font-inter" >Sumbit a Issue</h1> 
     <div className="max-w-2xl">
     <TextField.Root>
-        <TextField.Input  color="green" variant="soft" placeholder="Enter your issue"></TextField.Input>
+        <TextField.Input  color="green" variant="soft" value={title} placeholder="Enter your issue" onChange={(ev)=>{setTitle(ev.target.value)}}></TextField.Input>
     </TextField.Root>
     </div>
     <div>
-        <SimpleMDE placeholder="Describe the issue..."/>
+        <SimpleMDE placeholder="Describe the issue..." value={description} onChange={onChangeDescription}/>
     </div>
     </>
   )
